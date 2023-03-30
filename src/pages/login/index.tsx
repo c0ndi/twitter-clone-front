0336fc270a76b539c165beb9496855b7866ff339
Konrad <kowalik0konrad@gmail.com>
2023-03-30 23:18:55 +0200

    Docker setup

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             <div className="lg:w-1/2 lg:text-left">
                <h1 className="text-5xl font-bold">Login</h1>
                <p className={"py-6"}>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                </p>
             </div>
             <LoginForm/>
